7d471f69c4a26b33f417a9779fa3ecd05db489e9
mmagnoffsp <cmmf@yahoo.com>
2026-04-23 18:08:22 -0300

    feat: banco de dados conectado e rotas de navegação corrigidas

--- src/services/supabaseClient.js
+++ src/services/supabaseClient.js
@@ -1,9 +1,9 @@
 import { createClient } from '@supabase/supabase-js'
 
-// URL do seu novo projeto guardiao-pet-brasil
+// URL do seu projeto
 const supabaseUrl = 'https://clplerltyjbsvpjhchga.supabase.co'
 
-// Sua nova Publishable Key que você gerou agora
-const supabaseKey = 'sb_publishable_bOVOD3bLWKvAWFTSkFJkhg_XXGJP9KX'
+// Sua chave Anon que você acabou de me enviar
+const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImNscGxlcmx0eWpic3ZwamhjaGdhIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzY5NjcwOTQsImV4cCI6MjA5MjU0MzA5NH0.x0JNjiUNpukboQZuQuGpt6tbqL_upOf_Kk6L5Y938dA'
 
 export const supabase = createClient(supabaseUrl, supabaseKey)
