8931fa86d34964741ce270d86d424c9ae54d5218
mmagnoffsp <cmmf@yahoo.com>
2026-04-23 18:13:09 -0300

    feat: configurado supabase, corrigido roteamento e case-sensitivity

--- src/services/supabaseClient.js
+++ src/services/supabaseClient.js
@@ -1,9 +1,9 @@
-import { createClient } from '@supabase/supabase-js'
+import { createClient } from '@supabase/supabase-js';
 
-// URL do seu projeto
-const supabaseUrl = 'https://clplerltyjbsvpjhchga.supabase.co'
+// URL do projeto guardiao-pet-brasil
+const supabaseUrl = 'https://clplerltyjbsvpjhchga.supabase.co';
 
-// Sua chave Anon que você acabou de me enviar
-const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImNscGxlcmx0eWpic3ZwamhjaGdhIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzY5NjcwOTQsImV4cCI6MjA5MjU0MzA5NH0.x0JNjiUNpukboQZuQuGpt6tbqL_upOf_Kk6L5Y938dA'
+// Chave Anon (Public) para conexão client-side
+const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImNscGxlcmx0eWpic3ZwamhjaGdhIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzY5NjcwOTQsImV4cCI6MjA5MjU0MzA5NH0.x0JNjiUNpukboQZuQuGpt6tbqL_upOf_Kk6L5Y938dA';
 
-export const supabase = createClient(supabaseUrl, supabaseKey)
+export const supabase = createClient(supabaseUrl, supabaseKey);
