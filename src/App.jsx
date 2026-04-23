cff273aa40042e9c5a431dbf821523b696f484dc
mmagnoffsp <cmmf@yahoo.com>
2026-04-23 17:15:31 -0300

    feat: configurada rota /adotados e atualizado App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,21 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Navbar from './components/Navbar';
 import Home from './pages/Home';
-import CadastroPet from './components/CadastroPet'; // Vamos criar este arquivo no passo 2
+import CadastroPet from './components/CadastroPet';
+import CadastroAdotados from './components/cadastroadotados'; // Importando a nova ficha
 
 function App() {
   return (
     <Router>
       <Navbar />
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/cadastro" element={<CadastroPet />} />
-      </Routes>
+      <div className="container mx-auto mt-4"> {/* Container para centralizar o conteúdo */}
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/cadastro" element={<CadastroPet />} />
+          {/* Nova rota para a ficha de pets adotados */}
+          <Route path="/adotados" element={<CadastroAdotados />} />
+        </Routes>
+      </div>
     </Router>
   );
 }
