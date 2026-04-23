d259a0932de3564a41592742b429b12675277fd0
mmagnoffsp <cmmf@yahoo.com>
2026-04-23 18:30:18 -0300

    fix: recriado supabaseClient com biblioteca oficial e chaves configuradas

--- src/rm src/supabaseClient.js
+++ src/rm src/supabaseClient.js
@@ -7,4 +7,3 @@ const supabaseUrl = 'https://clplerltyjbsvpjhchga.supabase.co';
 const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImNscGxlcmx0eWpic3ZwamhjaGdhIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzY5NjcwOTQsImV4cCI6MjA5MjU0MzA5NH0.x0JNjiUNpukboQZuQuGpt6tbqL_upOf_Kk6L5Y938dA';
 
 export const supabase = createClient(supabaseUrl, supabaseKey);
-//teste
--- src/supabaseClient.js
+++
@@ -1,8 +0,0 @@
-import { createClient } from '@supabase/supabase-client';
-
-// Você encontra esses dados no painel do seu projeto Supabase 
-// em: Settings -> API
-const supabaseUrl = 'https://SUA_URL_AQUI.supabase.co';
-const supabaseAnonKey = 'SUA_CHAVE_ANON_AQUI';
-
-export const supabase = createClient(supabaseUrl, supabaseAnonKey);
